7fd026e60b5b88c38878c715778c0a222fcdca12
Julio Castillo <juliocastillo.sistemas@gmail.com>
2016-12-21 10:41:17 -0300

    splash screen

--- MJOMisionCba/app/src/main/java/com/mjo/misioncba/section/contact/ContactFragment.java
+++ MJOMisionCba/app/src/main/java/com/mjo/misioncba/section/contact/ContactFragment.java
@@ -1,14 +1,22 @@
 package com.mjo.misioncba.section.contact;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.mjo.misioncba.MainActivity;
 import com.mjo.misioncba.R;
 
 import java.util.ArrayList;
@@ -16,8 +24,9 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ContactFragment extends Fragment {
+public class ContactFragment extends Fragment implements ContactView.OnContactViewButtonsListener {
 
+    private static final int REQUEST_PHONE_CALL = 1;
     private LinearLayout contactContainer;
     private LinearLayout cottoContainer;
 
@@ -53,10 +62,55 @@ public class ContactFragment extends Fragment {
         for (ContactModel contact: contactModels) {
 
             ContactView contactView = new ContactView(getContext());
+            contactView.setmListener(this);
             contactView.configureForModel(contact);
 
             this.contactContainer.addView(contactView);
         }
     }
 
+    @Override
+    public void onCallButtonClick(String phoneNumber) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+            }
+            else
+            {
+                startActivity(intent);
+            }
+        }
+        else
+        {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onSendMsnButtonClick(String phoneNumber) {
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hola !! Como va ?");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PHONE_CALL: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+918511812660"));
+                    startActivity(intent);
+                }
+                return;
+            }
+        }
+    }
 }
--- MJOMisionCba/app/src/main/java/com/mjo/misioncba/section/contact/ContactView.java
+++ MJOMisionCba/app/src/main/java/com/mjo/misioncba/section/contact/ContactView.java
@@ -3,6 +3,7 @@ package com.mjo.misioncba.section.contact;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,8 +19,8 @@ public class ContactView  extends LinearLayout {
     private ImageButton buttonCall;
     private ImageButton buttonMsn;
     private TextView contactName;
-    private TextView contactPhone;
     private String contactPhoneString;
+    private OnContactViewButtonsListener mListener;
 
     public ContactView(Context context) {
         super(context);
@@ -36,11 +37,13 @@ public class ContactView  extends LinearLayout {
         init();
     }
 
+    public void setmListener(OnContactViewButtonsListener mListener) {
+        this.mListener = mListener;
+    }
 
     public void configureForModel (ContactModel model){
 
         this.contactName.setText(model.getContactName());
-        this.contactPhone.setText(model.getContactNumber());
         this.contactPhoneString = model.getContactNumber();
 
     }
@@ -51,8 +54,33 @@ public class ContactView  extends LinearLayout {
         inflater.inflate(R.layout.contact_view, this, true);
 
         this.contactName = (TextView)findViewById(R.id.contact_view_name);
-        this.contactPhone = (TextView)findViewById(R.id.contact_view_phone);
         this.buttonCall = (ImageButton)findViewById(R.id.contact_view_button_call);
         this.buttonMsn = (ImageButton)findViewById(R.id.contact_view_button_msn);
+        this.buttonCall.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if( mListener != null){
+                   mListener.onCallButtonClick(contactPhoneString);
+               }
+            }
+        });
+
+        this.buttonMsn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( mListener != null){
+                    mListener.onSendMsnButtonClick(contactPhoneString);
+                }
+            }
+        });
+    }
+
+
+
+    public interface OnContactViewButtonsListener {
+
+        void onCallButtonClick(String phoneNumber);
+
+        void onSendMsnButtonClick(String phoneNumber);
     }
 }
